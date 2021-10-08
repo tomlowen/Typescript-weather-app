85f12c9944a073221876a2e6dcdef9ecee4d4f44
corshidan <corshi.dan@gmail.com>
2021-10-08 15:30:18 +0100

    added clouds

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import CustomButton from "../CustomButton"
+import CustomButton from '../CustomButton';
 
 export default function WeatherCard({ city, handleClick }: any) {
 	return (
@@ -29,24 +29,48 @@ export default function WeatherCard({ city, handleClick }: any) {
 						<p className="capitalize">Pressure:{city.main.pressure}hPa</p>
 					</div>
 				</div>
-				<div className="font-semibold text-gray-800 text-5xl text-center lg:text-left px-2 mt-4 mb-2">
-					{city.name} {city.main.temp.toFixed(0)}
-					<sup> o</sup>C
+				<div className="flex justify-between px-2 mt-4 mb-2">
+					<div className="font-semibold text-gray-800 text-5xl text-center lg:text-left">
+						{' '}
+						{city.name} {city.main.temp.toFixed(0)}
+						<sup> o</sup>C
+					</div>
+					<div className="flex flex-col items-center justify-center">
+						<p>Cloud coverage </p>
+						<div className="relative">
+							{' '}
+							<img
+								style={{ opacity: `${city.clouds.all}%` }}
+								// style={{ opacity: '10%' }}
+								className="w-16"
+								src="/cloud-computing.png"
+								alt="clouds"
+							/>
+							<p className="absolute inset-1/3">{city.clouds.all}%</p>
+						</div>
+					</div>
 				</div>
 
-				<div className="text-gray-600 font-medium text-sm pt-1 text-center lg:text-left px-2">
-					Feels like -- {Math.floor(city.main.feels_like)}
-					<sup> o</sup>C
-				</div>
+				<div className="text-gray-600 font-medium text-sm pt-1 text-center lg:text-left px-2"></div>
 				<div className="text-gray-600 font-medium text-sm pt-1 text-center lg:text-left px-2">
 					Min -- {Math.floor(city.main.temp_min)}
-					<sup> o</sup>C Max --{Math.floor(city.main.temp_max)}
+					<sup> o</sup>C Max -- {Math.floor(city.main.temp_max)}
 					<sup> o</sup>C
 				</div>
 			</div>
 			<div className="flex flex-col justify-between w-full lg:w-1/4 bg-white px-2 lg:px-0">
-				<CustomButton buttonText="Clear" handleClick={()=>handleClick(city.name)}/>
-				<p>Hello</p>
+				<CustomButton buttonText="Clear" handleClick={() => handleClick(city.name)} />
+				<p>Wind speed: {city.wind.speed} m/s</p>
+				<p>Wind direction </p>
+				<div className="flex justify-around align-center">
+					<img className="w-14" src="/wind-sign.png" alt="flag" />
+					<img
+						style={{ transform: `rotate(${city.wind.deg - 90}deg)` }}
+						className="w-10"
+						src="/right-arrow.png"
+						alt="wind arrow"
+					/>
+				</div>
 			</div>
 		</div>
 	);
